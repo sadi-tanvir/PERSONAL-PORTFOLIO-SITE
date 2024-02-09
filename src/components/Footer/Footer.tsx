ba8c79c5e6a41ac8d2909b6dafaffa640091b93e
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2024-02-09 19:59:04 +0600

    feat(update): Make Responsive Footer Section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,13 +9,13 @@ import ME from "@/../public/me.png";
 const Footer = () => {
     return (
         <div className='w-full py-20 text-gray-300 bg-gray-800'>
-            <div className='container mx-auto grid grid-cols-3'>
-                <div className="">
+            <div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+                <div className="mx-auto mb-10 sm:mb-0 text-center sm:text-start">
                     <h2 className='text-xl font-bold uppercase'>Tanvir Hossain Sadi</h2>
                     <Image
                         src={ME}
                         alt='sadi'
-                        className='w-24'
+                        className='w-24 mx-auto sm:mx-0'
                     />
                     <p className='mt-1 text-[#2ed573]'>Full Stack Developer</p>
                     <p className='mt-1'>
@@ -27,8 +27,8 @@ const Footer = () => {
                         +880 1881-540890
                     </p>
                 </div>
-                <div className="space-y-3">
-                    <h3 className='text-xl underline capitalize'>profile links</h3>
+                <div className="space-y-3 mx-auto">
+                    <h3 className='text-xl underline capitalize text-[#2ed573]'>profile links</h3>
                     <a target='_blank' href="https://github.com/sadi-tanvir" className='flex items-center text-lg underline'>
                         <FaGithub
                             color='white'
@@ -53,8 +53,8 @@ const Footer = () => {
                     </a>
                 </div>
 
-                <div className='space-y-7 mt-12'>
-                    <div className='space-y-2'>
+                <div className='space-y-5 mt-12 mx-auto sm:col-span-2 md:col-span-1'>
+                    <div className='space-y-2 flex flex-col justify-center items-center'>
                         <p>
                             Designed & Developed by
                             <a className='text-blue-400 underline ml-2' target='_blank' href="https://www.linkedin.com/in/tanvir-hossain-sadi">
@@ -66,7 +66,7 @@ const Footer = () => {
                             2024, All Rights Reserved
                         </p>
                     </div>
-                    <p>Thanks for visiting my profile.</p>
+                    <p className='text-center'>Thanks for visiting my profile.</p>
                 </div>
             </div>
         </div>
